d36d4c9f11d4b5b8ce97d3af1e335152ccfae700
UNS2406 <71777971+UNS2406@users.noreply.github.com>
2022-11-10 00:20:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+//TREX GAme by **write your name ** using JS
+
+
+
+//Declare variables for game objects and behaviour indicators(FLAGS)
+
+
+//Create Media library and load to use it during the course of the software
+//executed only once at the start of the program
+function preload() {
+
+
+ 
+
+
+}
+
+//define the intial environment of the software(before it is used)
+//by defining the declared variables with default values
+//executed only once at the start of the program
+function setup() {
+
+
+
+}
+
+//All modifications, changes, conditions, manipulations, actions during the course of the program are written inside function draw.
+//All commands to be executed and checked continously or applied throughout the program are written inside function draw.
+//function draw is executed for every frame created since the start of the program.
+function draw() {
+
+
+
+
+}
